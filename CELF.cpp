98a53e603346de661e69ec73354b4833f1231b16
simp1eton <scq1993@gmail.com>
2015-11-16 22:10:11 -0800

    changes to CELF

--- CELF.cpp
+++ CELF.cpp
@@ -1,8 +1,14 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
+#include <set>
+#include <vector>
+#include <algorithm>
+#include <cstring>
+#include <queue>
 using namespace std;
 
 typedef long long int LL;
-const int INF = 1000000000;
+const int INF = 1000000000, NUM_ITERS = 10;
 vector <vector <int> > G;
 vector <vector <LL> > dist;
 vector <LL> delta, cost;
@@ -14,19 +20,23 @@ struct celf_cmp {
   bool operator() (int a, int b) {
     LL v1 = (uniCost) ? delta[a] : delta[a] * cost[b];
     LL v2 = (uniCost) ? delta[b] : delta[b] * cost[a];
-    if (v1 != v2) return v1 < v2;
+    if (v1 != v2) return v1 > v2;
     return a < b;
   }
 };
 
-LL eval(set <int> A) {
+LL eval(set <int> &A) {
+  if (A.size() == 0) return -INF;
   LL ret = 0;
   for (int i=0;i<N;++i) {
-    LL cur = 0;
-    for (auto a : A) cur = max(cur, dist[a][i]);
+    LL cur = INF;
+    for (auto a : A) cur = min(cur, dist[a][i]);
     ret += cur;
   }
-  return ret;
+//  printf("eval: %d, (", A.size());
+//  for (auto a : A) printf("%d ",a);
+//  printf(") is %lld\n",ret);
+  return -ret;
 }
 
 void preprocess() {
@@ -52,8 +62,9 @@ void preprocess() {
 void read(string filename) {
   FILE *fin = fopen(filename.c_str(), "r");
   fscanf(fin, "%d",&N);
+  delta = vector <LL>(N, INF);
+  cost = vector<LL>(N, 1);
   for (int i=0;i<N;++i) {
-    delta.push_back(0);
     vector <int> edges;
     int K, t;
     fscanf(fin, "%d",&K);
@@ -70,11 +81,14 @@ void read(string filename) {
 set<int> lazyForward(bool isUniCost, set<int> init = set<int>()) {
   uniCost = isUniCost;
   set <int, celf_cmp> remaining;
-  if (init.size() == 0)
+  if (init.size() == 0) {
+    for (int i=0;i<N;++i) delta[i] = INF;
     for (int i=0;i<N;++i) remaining.insert(i);
-  else
+  }
+  else {
+    for (int i : init) delta[i] = INF;
     for (int i : init) remaining.insert(i);
-  for (auto s : remaining) delta[s] = INF;
+  }
   LL remainingBudget = budget;
   set <int> ret;
   while (remainingBudget > 0) {
@@ -94,6 +108,7 @@ set<int> lazyForward(bool isUniCost, set<int> init = set<int>()) {
       }
       else break; 
     }
+    printf("%d\n",cur);
     remainingBudget -= (uniCost) ? 1 : cost[cur];
     ret.insert(cur);
   }
@@ -103,8 +118,26 @@ set<int> lazyForward(bool isUniCost, set<int> init = set<int>()) {
 set<int> CELF() {
   preprocess();
   set<int> A = lazyForward(1);
-  set<int> B = lazyForward(0);
-  return (eval(A) < eval(B)) ? A : B;
+  return A;
+  //set<int> B = lazyForward(0);
+  //return (eval(A) < eval(B)) ? A : B;
+}
+
+set <int> randomize() {
+  int x = NUM_ITERS;
+  preprocess();
+  set <int> ret;
+  vector <int> A;
+  for (int i=0;i<N;++i) A.push_back(i);
+  while (x--) {
+    random_shuffle(A.begin(), A.end());
+    set <int> init;
+    for (int i=0;i<10 * budget; ++i) init.insert(A[i]);
+    set <int> cur = lazyForward(1, init);
+    if (eval(cur) > eval(ret)) ret = cur;
+    printf("val = %lld\n",eval(cur));
+  }
+  return ret;
 }
 
 int main () {
@@ -112,4 +145,9 @@ int main () {
   cin >> s;
   read(s);
   set <int> sol = CELF();
+  printf("value = %lld\n",eval(sol));
+  for (auto s : sol) printf("%d ",s); printf("\n");
+  sol = randomize();
+  printf("value = %lld\n",eval(sol));
+  for (auto s : sol) printf("%d ",s); printf("\n");
 }
