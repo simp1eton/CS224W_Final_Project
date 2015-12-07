86ad69e8e583d58ffde6d6350636e09b96fce101
Qianying Lin <qlin1@stanford.edu>
2015-12-06 23:25:11 -0800

    forest and stochastic

--- DATA/generate.sh
+++ DATA/generate.sh
@@ -2,9 +2,9 @@
 v=("erdos_renyi_1.py" "Barabasi-Albert_sample_2.py" "configuration_model_3.py" "geometric_configuration_4.py" "forest_fire_5.py" "stochastic_block_model_6.py")
 for i in `seq 1 50`;
 do 
-  python ${v[4]} > input5-${i}-${v[4]}.txt 
+  python ${v[4]} > input5-${i}.txt 
 done
 for i in `seq 1 50`;
 do 
-  python ${v[5]} > input6-${i}-${v[5]}.txt 
+  python ${v[5]} > input6-${i}.txt 
 done
