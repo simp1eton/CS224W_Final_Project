770b4a32f8c044b3b499e04fc1b54069cac40c0e
Kaidi Yan <kaidi@stanford.edu>
2015-12-07 18:50:41 -0800

    updated degree

--- PYTHON/degree.py
+++ PYTHON/degree.py
@@ -3,35 +3,59 @@ from util import (
   MaxHeap
 )
 import time
-
-G = read_file("blah.txt")
-G.preprocess()
-PQ = MaxHeap()
-
-for i in range(G.N):
-  PQ.update(i, G.N * G.N)
-
-S = set()
-cur_val = G.N * G.N
+import numpy
+import sys
 
 budget = 20
+graph_type = int(sys.argv[1])
 
-used = set()
+stats = [[] for _ in range(budget)]
+ans = [[] for _ in range(budget)]
 
-start_time = time.time()
-for _ in range(budget):
-  while True:
-    cur, val, aux = PQ.pop()
-    if cur == -1: break
-    if cur in used: continue
-    new_val = G.avg_dist(S.union(set([cur]))) 
-    if cur_val - new_val == val:
-      S.add(cur)
-      cur_val = new_val
-      used.add(cur)
-      for y in G.G[cur]: used.add(y)
-      break
-    PQ.update(cur, cur_val - new_val)
-  end_time = time.time()
-  #print S
-  print end_time - start_time, cur_val
+for i in range(1, 51):
+    filename = "../DATA/mass_data/input" + `graph_type` + "-" + `i` + ".txt"
+    G = read_file(filename)
+    G.preprocess()
+    PQ = MaxHeap()
+
+    for i in range(G.N):
+      PQ.update(i, G.N * G.N)
+
+    S = set()
+    cur_val = G.N * G.N
+
+    used = set()
+
+    start_time = time.time()
+    for curr_budget in range(budget):
+      while True:
+        cur, val, aux = PQ.pop()
+        if cur == -1: break
+        if cur in used: continue
+        new_val = G.avg_dist(S.union(set([cur]))) 
+        if cur_val - new_val == val:
+          S.add(cur)
+          cur_val = new_val
+          used.add(cur)
+          for y in G.G[cur]: used.add(y)
+          break
+        PQ.update(cur, cur_val - new_val)
+      end_time = time.time()
+      total_time = end_time - start_time
+      avg_dist = cur_val
+      #print S
+      print total_time, avg_dist
+      stats[curr_budget].append(total_time)
+      ans[curr_budget].append(avg_dist)
+
+
+print '=' * 50
+for i in range(budget):
+    stats_list = stats[i]
+    ans_list = ans[i]
+    stats_std = numpy.std(stats_list)
+    stats_mean = numpy.mean(stats_list)
+    ans_std = numpy.std(ans_list)
+    ans_mean = numpy.mean(ans_list)
+    print stats_mean, stats_std, ans_mean, ans_std
+
