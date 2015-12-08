89834ef0485964b300b1aab6f172d7f0f723191e
simp1eton <scq1993@gmail.com>
2015-12-07 22:39:44 -0800

    generated all results

---
+++ PYTHON/run_algo.sh
@@ -0,0 +1,6 @@
+python $1.py 1 > ../RESULTS/$11.txt
+python $1.py 2 > ../RESULTS/$12.txt
+python $1.py 3 > ../RESULTS/$13.txt
+python $1.py 4 > ../RESULTS/$14.txt
+python $1.py 5 > ../RESULTS/$15.txt
+python $1.py 6 > ../RESULTS/$16.txt
